0709b1805c718a64b9438a40e27821d8c1ef42ef
Ashwin <ash123.8055@gmail.com>
2025-02-21 14:56:44 +0530

    [Ashwin] Add: Added the review-3 codes.

--- Review-3/2_swap_number.py
+++ Review-3/2_swap_number.py
@@ -1,8 +1,12 @@
+def swap_pair(num_list):
+    for counter in range(0,len(num_list)-2,4):
+    num_list[counter],num_list[counter+2]=num_list[counter+2],num_list[counter]
+    num_list[counter+1],num_list[counter+3]=num_list[counter+3],num_list[counter+1]
+    return num_list
+
+
 list_len=int(input("Enter the number of elements in list: "))    
 num_list=[int(input("Enter the number: ")) for counter in range(list_len)]
 print(num_list)
-for counter in range(0,len(num_list)-2,4):
-    num_list[counter],num_list[counter+2]=num_list[counter+2],num_list[counter]
-    num_list[counter+1],num_list[counter+3]=num_list[counter+3],num_list[counter+1]
     
-print(num_list)
+swap_pair(num_list)
